eb8bceaf3106bfdcb9731c73e36d6ab0486ff2c5
Hack Reactor Student <student@hackreactor.com>
2017-06-12 16:20:40 -0700

    generated schema

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,18 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
+});
 
+connection.connect();
+
+connection.query('SELECT * from messages', function (err, rows, fields) {
+  if (err) { throw err; }
+  console.log('Query test succedeed');
+});
+
+connection.end();
--- server/schema.sql
+++ server/schema.sql
@@ -4,9 +4,21 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+    ID int AUTO_INCREMENT,
+    Message varchar(255) NOT NULL,
+    Roomname varchar(255) NOT NULL,
+    Uid int NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (Uid) REFERENCES users(ID)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(255),
+    PRIMARY KEY (ID)
+);
 
 
 
